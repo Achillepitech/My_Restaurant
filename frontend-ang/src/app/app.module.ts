5a4f3bdc4806716e6adeea6555a74fc0352e408a
ACHILLE ZOUAOUI <ABULYGINE@Atecna-Designer.lan>
2025-01-10 20:54:08 +0100

    linked front & def func & jsonignore ok nexst step menu ignored link with plats

--- frontend-ang/src/app/app.module.ts
+++ frontend-ang/src/app/app.module.ts
@@ -3,21 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AdmniTemplateComponent } from './admni-template/admni-template.component';
+
+// Importation des modules Angular Material nécessaires
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import { HomeComponent } from './home/home.component';
+import { DescriptionComponent } from './description/description.component';
+import { InfoComponent } from './info/info.component';
+import {MatDrawer, MatDrawerContainer} from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdmniTemplateComponent
+    AdmniTemplateComponent,
+    HomeComponent,
+    DescriptionComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDrawer,
+    MatDrawerContainer
+
+
   ],
-  providers: [
-    provideAnimationsAsync()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
